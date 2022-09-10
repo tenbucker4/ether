65def85954ea55de577cf2e43a21a01c7869c85a
Ben Tucker <bwtucker4@gmail.com>
2022-09-10 12:54:17 -0700

    Added transitions to planet toggling

--- src/App.js
+++ src/App.js
@@ -13,20 +13,52 @@ const planets = [
     "pluto",
 ];
 
+const colors = [
+    "#e8927c",
+    "#b45d15",
+    "#26daaa",
+    "#e55f45",
+    "orange",
+    "#b29d81",
+    "#8dcdd8",
+    "#4f83e2",
+    "#fccfba",
+];
+
 function App() {
     const [index, setIndex] = useState(0);
     const [currentPlanet, setCurrentPlanet] = useState(planets[index]);
+    const [currentColor, setCurrentColor] = useState(colors[index]);
     const [opacity, setOpacity] = useState(1);
 
-    const translateRight = async () => {
+    const translateRight = () => {
+        timer();
+        // console.log(opacity);
+        // setOpacity(0);
+        // console.log(opacity);
         setIndex((prev) => prev + 1);
-        console.log(index);
-        setCurrentPlanet(planets[index + 1]);
-        console.log(currentPlanet);
+        setTimeout(() => {
+            setCurrentPlanet(planets[index + 1]);
+            setCurrentColor(colors[index + 1]);
+        }, 1500);
+        // setCurrentPlanet(planets[index + 1]);
+        // setCurrentColor(colors[index + 1]);
+        // setOpacity(1);
     };
 
     const translateLeft = () => {
+        // setOpacity(0);
         setIndex((prev) => prev - 1);
+        setCurrentPlanet(planets[index - 1]);
+        setCurrentColor(colors[index - 1]);
+        // setOpacity(1);
+    };
+
+    const timer = () => {
+        setOpacity(0);
+        setTimeout(() => {
+            setOpacity(1);
+        }, 2500);
     };
 
     return (
@@ -51,11 +83,17 @@ function App() {
                 style={{
                     textTransform: "uppercase",
                     fontWeight: "100",
+                    opacity: `${opacity}`,
+                    transition: "opacity 0.5s ease-in-out",
                 }}
             >
                 <div style={{ fontSize: "24px" }}>{currentPlanet}</div>
                 <div
-                    style={{ fontSize: "14px", color: "#e8927c" }}
+                    style={{
+                        fontSize: "14px",
+                        color: `${currentColor}`,
+                        textDecoration: "underline",
+                    }}
                     className="learn-more"
                 >
                     Learn More
