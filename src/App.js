7c2847614919174b29ecda16615b4fad5f946517
Ben Tucker <bwtucker4@gmail.com>
2022-09-10 00:37:27 -0700

    Format shapes and colors of planets

--- src/App.js
+++ src/App.js
@@ -15,15 +15,15 @@ function App() {
                         transform: `translateX(calc(${index} * calc(-100% / 9)))`,
                     }}
                 >
-                    <div className="box"></div>
-                    <div className="box"></div>
-                    <div className="box"></div>
-                    <div className="box"></div>
-                    <div className="box"></div>
-                    <div className="box"></div>
-                    <div className="box"></div>
-                    <div className="box"></div>
-                    <div className="box"></div>
+                    <div className="test mercury"></div>
+                    <div className="test venus"></div>
+                    <div className="test earth"></div>
+                    <div className="test mars"></div>
+                    <div className="test jupiter"></div>
+                    <div className="test saturn"></div>
+                    <div className="test uranus"></div>
+                    <div className="test neptune"></div>
+                    <div className="test pluto"></div>
                 </div>
             </div>
         </>
